e592a019e78b1d7ee8bef95a577ac55972d7e56e
adrianpura <pura.adriano@gmail.com>
2022-08-02 20:30:37 +0800

    crud rest api
    Signed-off-by: adrianpura <pura.adriano@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemsController } from './items/items.controller';
+import { ItemsModule } from './items/items.module';
+import { ItemsService } from './items/items.service';
+import config from './config/keys';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ItemsModule, MongooseModule.forRoot(config.mongoURI)],
+  controllers: [AppController, ItemsController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoURI:
+    'mongodb+srv://test:test@cluster0.syktkwz.mongodb.net/?retryWrites=true&w=majority',
+};
